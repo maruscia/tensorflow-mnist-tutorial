Added 5-Layer NN implementation with ReLU units (fixed learning rate, AdamOptimizer). Minor UI tweaks. Minor refactoring.

mnist_2.1_five_layers_relu.py
@@ -22,15 +22,15 @@ tf.set_random_seed(0)
 # neural network with 5 layers
 #
 # · · · · · · · · · ·          (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
-# \x/x\x/x\x/x\x/x\x/       -- fully connected layer (sigmoid)      W1 [784, 200]      B1[200]
+# \x/x\x/x\x/x\x/x\x/       -- fully connected layer (relu)      W1 [784, 200]      B1[200]
 #  · · · · · · · · ·                                                Y1 [batch, 200]
-#   \x/x\x/x\x/x\x/         -- fully connected layer (sigmoid)      W2 [200, 100]      B2[100]
+#   \x/x\x/x\x/x\x/         -- fully connected layer (relu)      W2 [200, 100]      B2[100]
 #    · · · · · · ·                                                  Y2 [batch, 100]
-#     \x/x\x/x\x/           -- fully connected layer (sigmoid)      W3 [100, 60]       B3[60]
+#     \x/x\x/x\x/           -- fully connected layer (relu)      W3 [100, 60]       B3[60]
 #      · · · · ·                                                    Y3 [batch, 60]
-#       \x/x\x/             -- fully connected layer (sigmoid)      W4 [60, 30]        B4[30]
+#       \x/x\x/             -- fully connected layer (relu)      W4 [60, 30]        B4[30]
 #        · · ·                                                      Y4 [batch, 30]
-#         \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
+#         \x/               -- fully connected layer (relu)      W5 [30, 10]        B5[10]
 #          ·                                                        Y5 [batch, 10]
 
 # Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
@@ -47,24 +47,23 @@ M = 100
 N = 60
 O = 30
 # Weights initialised with small random values between -0.2 and +0.2
-# When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
 W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
-B1 = tf.Variable(tf.zeros([L]))
+B1 = tf.Variable(tf.ones([L])/10)
 W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
-B2 = tf.Variable(tf.zeros([M]))
+B2 = tf.Variable(tf.ones([M])/10)
 W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
-B3 = tf.Variable(tf.zeros([N]))
+B3 = tf.Variable(tf.ones([N])/10)
 W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
-B4 = tf.Variable(tf.zeros([O]))
+B4 = tf.Variable(tf.ones([O])/10)
 W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
 B5 = tf.Variable(tf.zeros([10]))
 
 # The model
 XX = tf.reshape(X, [-1, 784])
-Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
-Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
-Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
-Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+Y1 = tf.nn.relu(tf.matmul(XX, W1) + B1)
+Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
 Ylogits = tf.matmul(Y4, W5) + B5
 Y = tf.nn.softmax(Ylogits)
 
@@ -87,8 +86,8 @@ datavis = tensorflowvisu.MnistDataVis()
 
 # training step, learning rate = 0.003
 learning_rate = 0.003
-#FIXME: use a better optimizer that avoids saddle points (AdamOptimizer)
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
 # init
 init = tf.global_variables_initializer()